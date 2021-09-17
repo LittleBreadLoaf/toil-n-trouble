6bf4c662dd7bbb68ca656b14da5d99d72b89181d
Emer <33635787+ERBirch01@users.noreply.github.com>
2021-09-16 21:52:31 -0600

    Added simple pause menu toggled by Tab. Will need to connect to Main Menu when one is made.

---
+++ Assets/Scripts/GameScreens/PauseManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private bool isPaused;
+    public GameObject pausePanel;
+    public string mainMenu;
+
+    void Start()
+    {
+        isPaused = false;
+        pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("pause"))
+        {
+            ChangePause();
+        }
+
+    }
+
+    public void ChangePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void QuitToMain()
+    {
+        SceneManager.LoadScene(mainMenu);
+        Time.timeScale = 1f;
+    }
+    
+}
